f89a8c1004650f06216f804bc93a29d4cbbcc54d
yang.guo <guoyanggit@gmail.com>
2022-10-25 18:17:43 +0800

    加入数据库和MobSDK

---
+++ mvvm/src/main/java/com/huafang/mvvm/db/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.huafang.mvvm.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.huafang.mvvm.entity.UserEntity
+
+/**
+ * @author yang.guo on 2022/10/25
+ * @describe
+ */
+@Database(entities = [UserEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        fun create(appContext: Context): AppDatabase {
+            val dbName = ""
+            return Room.databaseBuilder(
+                appContext.applicationContext,
+                AppDatabase::class.java,
+                dbName
+            ).build()
+        }
+    }
+
+    abstract fun userDao(): UserDao
+}
---
+++ mvvm/src/main/java/com/huafang/mvvm/db/UserDao.kt
@@ -0,0 +1,30 @@
+package com.huafang.mvvm.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.huafang.mvvm.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * @author yang.guo on 2022/10/25
+ * @describe
+ */
+@Dao
+interface UserDao {
+    @Query("select * from user_entity")
+    fun getAll(): Flow<List<UserEntity>>
+
+    @Query("select * from user_entity where userID in (:userIds)")
+    fun loadAllByIds(userIds: IntArray): Flow<List<UserEntity>>
+
+    @Query("select * from user_entity where userName like :userName and age like :age limit 1")
+    fun findByName(userName: String, age: Int): Flow<UserEntity>
+
+    @Insert
+    fun insertAll(vararg userEntity: UserEntity)
+
+    @Delete
+    fun delete(userEntity: UserEntity)
+}
